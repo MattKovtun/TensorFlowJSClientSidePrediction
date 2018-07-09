9979648f43278ed5cbde2dc30623b66994fef0c8
matt.kovtun <kovtun_m@ucu.edu.ua>
2018-07-09 16:51:10 +0300

    Added gitattributes, refactored interface

--- static/frontend/main.js
+++ static/frontend/main.js
@@ -5,11 +5,12 @@ const modelURL = 'http://localhost:5000/model';
 const preview = document.getElementById("preview");
 const predictButton = document.getElementById("predict");
 const clearButton = document.getElementById("clear");
-
+const numberOfFiles = document.getElementById("number-of-files");
+const fileInput = document.getElementById('file');
 
 const predict = async (modelURL) => {
     if (!model) model = await tf.loadModel(modelURL);
-    let files = document.getElementById('file').files;
+    let files = fileInput.files;
 
     [...files].map(async (img) => {
         const data = new FormData();
@@ -43,5 +44,7 @@ const renderImageLabel = (img, label) => {
 };
 
 
+
+fileInput.addEventListener("change", () => numberOfFiles.innerHTML = "Selected " + fileInput.files.length + " files", false);
 predictButton.addEventListener("click", () => predict(modelURL));
 clearButton.addEventListener("click", () => preview.innerHTML = "");
